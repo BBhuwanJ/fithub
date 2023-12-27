24a09a9575297cb0c426ff8aee56c6fba19375e5
BBhuwanJ <104883523+BBhuwanJ@users.noreply.github.com>
2023-12-27 19:47:51 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,188 +1,45 @@
-* {
-    margin: 0px;
-    padding: 0px;
-    box-sizing: border-box;
-}
+let themeBtn = document.querySelectorAll(".themeDark");
+let dropMenuBars = document.querySelector(".dropMenuBars");
+let dropDown = document.querySelector(".dropDown");
+let dropMenuIcon = document.getElementById("dropMenuIcon");
 
-:root {
-    ---primaryColor: #F3F3FD;
-    ---secondaryColor: black;
-    ---btncolor: white;
-}
+themeBtn[0].addEventListener("click", () => {
+  if (document.body.classList.contains("darkTheme")) {
+    lightTheme();
+    themeBtn[0].src = "images/moon.svg";
+    themeBtn[1].src = "images/moon.svg";
+  } else {
+    darkTheme();
+    themeBtn[0].src = "images/sun.svg";
+    themeBtn[1].src = "images/sun.svg";
+  }
+});
 
-.darkTheme {
-    ---primaryColor: black;
-    ---secondaryColor: white;
-    ---btncolor: white;
-}
+themeBtn[1].addEventListener("click", () => {
+  if (document.body.classList.contains("darkTheme")) {
+    lightTheme();
+    themeBtn[1].src = "images/moon.svg";
+  } else {
+    darkTheme();
+    themeBtn[1].src = "images/sun.svg";
+  }
+});
 
-body {
-    height: 100%;
-    width: 100%;
-    background-color: var(---primaryColor);
-    font-family: 'Inter', sans-serif;
-    ;
-}
+const lightTheme = () => {
+  document.body.classList.remove("darkTheme");
+  document.getElementById("dropMenuIcon").style.filter = "none";
+};
 
-nav {
-    margin-top: 20px;
-    display: flex;
-    align-items: center;
-    height: 67px;
-    /* border: 2px solid black; */
-    flex-direction: row;
-    justify-content: space-around;
-}
+const darkTheme = () => {
+  document.body.classList.add("darkTheme");
+  document.getElementById("dropMenuIcon").style.filter = "invert(100%)";
+};
 
-.logo {
-
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    /* border: 2px solid red; */
-    height: 67px;
-    min-width: 20%;
-}
-
-.logo img {
-    width: 215px;
-    cursor: pointer;
-    overflow: hidden;
-}
-
-.navList {
-    padding: 0 5vw;
-    /* border: 2px solid blue; */
-    height: 67px;
-    width: 45%;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
-    overflow: hidden;
-}
-
-.navList li {
-    padding: 0px 15px;
-    text-decoration: none;
-    list-style: none;
-    font-size: 1.3rem;
-    font-weight: 600;
-    color: var(---secondaryColor);
-    cursor: pointer;
-
-}
-
-.loginReg {
-    overflow: hidden;
-    padding: 0px 15px;
-    /* border: 2px solid red; */
-    display: flex;
-    align-items: center;
-    justify-content: right;
-    padding-right: 11px;
-    gap: 10px;
-    width: 20%;
-    height: 67px;
-}
-
-.loginReg button {
-    background: linear-gradient(90deg, #3A8EF6 0%, #6F3AFA 95.69%);
-    padding: 6px 17px;
-    font-weight: 500;
-    color: var(---btncolor);
-    border-radius: 6px;
-    border: none;
-    font-size: 1.7vw;
-    cursor: pointer;
-    overflow: hidden;
-    text-align: center;
-
-}
-
-.theme {
-    overflow: hidden;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    /* border: 2px solid red; */
-    width: 46px;
-    height: 67px;
-
-}
-
-.theme img {
-    cursor: pointer;
-}
-
-.dropDown {
-
-    position: absolute;
-    right: 0px;
-    max-width: 255px;
-    /* border: 2px solid red; */
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    float: right;
-    width: 60vw;
-    gap: 5px;
-    display: none;
-}
-
-.dropDown li {
-    padding: 0px 15px;
-    text-decoration: none;
-    list-style: none;
-    font-size: 1.3rem;
-    font-weight: 600;
-    color: var(---secondaryColor);
-    cursor: pointer;
-    list-style: none;
-
-}
-
-.open {
-    display: flex;
-}
-
-.hideOnScreen {
-    display: none;
-}
-
-.dropDown button {
-    background: linear-gradient(90deg, #3A8EF6 0%, #6F3AFA 95.69%);
-    padding: 6px 17px;
-    font-weight: 500;
-    color: var(---btncolor);
-    border-radius: 6px;
-    border: none;
-    font-size: 1.7vw;
-    cursor: pointer;
-    overflow: hidden;
-    text-align: center;
-    width: 100%;
-}
-
-.dropMenuBars {
-    max-width: 90vw;
-    float: right;
-}
-
-@media (max-width: 800px) {
-    .hideOnMobile {
-        display: none;
-    }
-
-    nav {
-        display: flex;
-        justify-content: space-between;
-        padding: 0px 25px;
-    }
-
-    .dropMenuBars {
-        max-width: 90vw;
-        display: block;
-    }
-}
+dropMenuBars.addEventListener("click", () => {
+  dropDown.classList.toggle("open");
+  if (dropDown.classList.contains("open")) {
+    dropMenuIcon.src = "images/close.svg";
+  } else {
+    dropMenuIcon.src = "images/menu.svg";
+  }
+});
